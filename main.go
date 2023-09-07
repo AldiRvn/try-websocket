e67ee791eb6560bee76a5c59ce5aef31c62fef3e
Arawan <45452761+AldiRvn@users.noreply.github.com>
2023-09-07 21:42:13 +0700

    Update main.go
    Example with for loop

--- main.go
+++ main.go
@@ -43,7 +43,7 @@ func main() {
 	// ? Fixing 403: https://stackoverflow.com/a/23324279
 	// wsServer := websocket.Server{Handler: websocket.Handler(EchoServer)}
 	// wsServer := websocket.Server{Handler: websocket.Handler(CountingServer)}
-	wsServer := websocket.Server{Handler: websocket.Handler(BindToJsonServer)}
+	// wsServer := websocket.Server{Handler: websocket.Handler(BindToJsonServer)}
 
 	//* -------------------------------- NET HTTP -------------------------------- */
 	// ? Ref: https://pkg.go.dev/golang.org/x/net/websocket
@@ -58,6 +58,20 @@ func main() {
 	//? Ref: https://github.com/gin-gonic/gin/issues/51#issuecomment-48201747
 	r := gin.New()
 	r.GET("/ws", func(c *gin.Context) {
+		wsServer := websocket.Server{Handler: websocket.Handler(func(ws *websocket.Conn) {
+			for {
+				select {
+				case <-c.Request.Context().Done(): //? Disconnect if the client is done connecting
+				default:
+					param := map[string]any{}
+					if err := json.NewDecoder(ws).Decode(&param); err != nil {
+						log.Println(err)
+						return
+					}
+					ws.Write([]byte(param)) //? Send Response
+				}
+			}
+		})}
 		wsServer.ServeHTTP(c.Writer, c.Request)
 	})
 	r.Run(":8080")
